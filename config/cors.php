dcb9181bf540e134d82391c08ea73e2e2929a110
sauravdhaka <shubham.shinde@invezzatechnologies.com>
2024-11-25 17:11:33 +0530

    cors fixed

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,12 @@ return [
     |
     */
 
-    'paths' => ['graphql/*'],
+    'paths' => ['/*'],
 
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS')),
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['X-Requested-With', 'Content-Type', 'X-Token-Auth', 'Authorization', 'Accept', 'X-Client-Key', 'X-Platform-User-Identifier-Key'],
+    'allowed_methods' => ['*'],
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
-
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
