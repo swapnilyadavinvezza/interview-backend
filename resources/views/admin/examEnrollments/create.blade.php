e9647b549ff575360f5098176f39209589eb091e
Rohit Dokhe <rohit.dokhe@invezzatechnologies.com>
2024-12-12 12:45:09 +0530

    removed the commented code

--- resources/views/admin/examEnrollments/create.blade.php
+++ resources/views/admin/examEnrollments/create.blade.php
@@ -27,19 +27,6 @@
                             @csrf
                             <div class="card-body">
                                 <div class="row">
-                                    {{-- @if ($errors->any())
-                                    <div class="col-md-8">
-                                        <div class="form-group">
-                                        <div class="alert alert-danger">
-                                            <ul>
-                                                @foreach ($errors->all() as $error)
-                                                    <li>{{ $error }}</li>
-                                                @endforeach
-                                            </ul>
-                                        </div>
-                                        </div>
-                                    </div>
-                                    @endif --}}
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label for="user_id">Users<span style="color: red;">*</span></label>
